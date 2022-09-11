d0597d59b0eabff65544971c4a831866d5de4266
vitortorres2008 <84298699+vitortorres2008@users.noreply.github.com>
2022-09-11 20:54:20 -0300

    Delete primeiro-projeto directory

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'meu-primeiro-component',
-  template: `
-    <p>Meu primeiro component com Angular 2!</p>
-  `
-})
-export class MeuPrimeiroComponent { }
